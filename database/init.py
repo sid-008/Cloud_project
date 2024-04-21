23689a4259efe736d6b3dc924840622a18d93c8f
Siddharth Tewari <siddharthtewari14@gmail.com>
2024-04-21 21:49:49 +0530

    Add: db init

---
+++ database/init.py
@@ -0,0 +1,47 @@
+# This script is to be run only if the
+# db does not exist
+
+import sqlite3 as db
+
+conn = db.connect("./cc.db")
+
+c = conn.cursor()
+
+c.execute("""create table if not exists users
+(
+id integer primary key,
+name text not null,
+email text not null unique,
+address text,
+hash text
+)""")
+
+c.execute("""create table if not exists products 
+(
+id integer primary key,
+name text not null,
+description text not null,
+price integer
+)
+""")
+
+c.execute("""create table if not exists orders
+(
+id integer primary key,
+user_id integer,
+product_id integer,
+foreign key (user_id)
+    references users(id)
+foreign key(product_id)
+    references products(id)
+)
+""")
+
+# c.execute("""create table stocks
+# (date text, trans text, symbol text,
+#  qty real, price real)""")
+
+
+conn.commit()
+
+c.close()
